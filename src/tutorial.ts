6d6fe6d087f1e97f531e2937a4f072f91467c926
Irina Saveleva <goyalion17@gmail.com>
2024-03-01 20:38:21 +0100

    typescript + vite

---
+++ src/tutorial.ts
@@ -0,0 +1,12 @@
+console.log("Typescript Practices");
+interface someValue {
+  name: string;
+  id: number;
+}
+
+let someObj: someValue = {
+  name: "iryna",
+  id: 159,
+};
+
+console.log(someObj);
